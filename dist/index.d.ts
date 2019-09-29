71577eab33d800e67ed6b078dc9bfc431f5b7d3b
ohsung cho <fifthsage@gmail.com>
2019-09-30 00:56:09 +0900

    add consumer

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,14 +1,15 @@
-/// <reference types="react" />
+import React from "react";
 interface AmplitudeProviderProps {
     logEvent: Function;
     event: LogEventProps | null;
 }
 interface LogEventProps {
-    key: string;
+    key: string | null;
     payload: {
         [key: string]: any;
     };
 }
 export declare const ProvideAmplitude: (props: any) => JSX.Element;
+export declare const ConsumeAmplitude: React.ExoticComponent<React.ConsumerProps<AmplitudeProviderProps>>;
 export declare const useAmplitude: () => AmplitudeProviderProps;
 export {};
--- dist/index.js
+++ dist/index.js
@@ -14,18 +14,21 @@ if (typeof window !== "undefined") {
 }
 var instance = amplitude ? amplitude.getInstance() : null;
 var amplitudeContext = react_1.createContext({
-    logEvent: function () { },
+    logEvent: Function,
     event: null
 });
 function useProvideAmplitude() {
-    var _a = react_1.useState(null), event = _a[0], setEvent = _a[1];
+    var _a = react_1.useState({
+        key: null,
+        payload: {}
+    }), event = _a[0], setEvent = _a[1];
     var logEvent = function (key, payload) {
         if (instance) {
             instance.logEvent(key, payload);
-            setEvent(function () { return ({
+            setEvent({
                 key: key,
                 payload: payload
-            }); });
+            });
         }
         return;
     };
@@ -33,18 +36,28 @@ function useProvideAmplitude() {
         return;
     }, []);
     return {
-        logEvent: logEvent,
-        event: event
+        event: event,
+        logEvent: logEvent
     };
 }
 exports.ProvideAmplitude = function (props) {
     var apiKey = props.apiKey, _a = props.userId, userId = _a === void 0 ? null : _a, _b = props.config, config = _b === void 0 ? {} : _b, children = props.children;
+    var defaultOptions = {
+        saveEvents: true,
+        includeUtm: true,
+        includeReferrer: true,
+        trackingOptions: {
+            city: false,
+            ip_address: false
+        }
+    };
     if (instance) {
-        instance.init(apiKey, userId, config);
+        instance.init(apiKey, userId, Object.assign(defaultOptions, config));
     }
     var _amplitude = useProvideAmplitude();
     return (react_1.default.createElement(amplitudeContext.Provider, { value: _amplitude }, children));
 };
+exports.ConsumeAmplitude = amplitudeContext.Consumer;
 exports.useAmplitude = function () {
     return react_1.useContext(amplitudeContext);
 };
